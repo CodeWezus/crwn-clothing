f8c748c120d0c4646b920c73e9cb40f0172c1522
CodeWezus <tseguin@lixar.com>
2021-10-22 14:33:04 -0400

    Homepge has been broken into smaller components. Added styling to menu items.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomePage/homepage.jsx';
+import HomePage from './Pages/HomePage/homepage.jsx';
 
 function App() {
   return (
