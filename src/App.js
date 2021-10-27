5047f2eaa808cd75041da6e23ed3c8d997791d6c
CodeWezus <tseguin@lixar.com>
2021-10-27 01:29:00 -0400

    Added a Shop page to the application and created collection components for product display

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/homepage.jsx';
-
-
-const HatsPage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './Pages/Shop/shop'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact='true' path='/shop/hats' component={HatsPage} />
+        <Route exact='true' path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
