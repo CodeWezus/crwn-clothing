7ac082b881205ad3c2060244fb8e6be9169704b4
CodeWezus <tseguin@lixar.com>
2021-10-26 23:51:40 -0400

    Added routing to the menu items using react-router-dom withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/homepage.jsx';
+
+
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact='true' path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
