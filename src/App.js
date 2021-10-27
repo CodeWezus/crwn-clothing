84247aa011a5b4ccbf101a53bd4bed86fa801738
CodeWezus <tseguin@lixar.com>
2021-10-27 01:50:58 -0400

    Added a header component which contains an SVG logo and links to Home, Shop and Contact pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/homepage.jsx';
 import ShopPage from './Pages/Shop/shop'
+import Header from './Components/header/header.jsx'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact='true' path='/shop' component={ShopPage} />
