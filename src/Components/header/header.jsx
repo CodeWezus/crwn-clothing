06ef1454c6f7beb2b8c5d5f7d3b959c0710fce4d
CodeWezus <tseguin@lixar.com>
2021-12-03 14:51:05 -0500

    Created cart-icon and cart-dropdown components for the application. Implemented cart reducers.

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../Firebase/firebase.utils'
 import { connect } from 'react-redux';
-
+import CartIcon from '../cart-icon/cart-icon';
 import './header.scss'
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -21,12 +22,18 @@ const Header = ({ currentUser }) => (
                     <Link className='option' to='/login'>LOG IN</Link>
                 )
             }
+        <CartIcon />
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 // Here we create a higher order component which allows us to attach our userReducer to the Header so the header can access the currentUser property.
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart-reducer';
 
 // The Root Reducer is a reducer that combines the data from all the reducers (states) in our application. This allows us to create a single consolidated state object for our application.
 // Essentailly we are making the state of out application more modular.
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
