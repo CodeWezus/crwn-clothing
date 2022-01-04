91c965234315f164453b83982314e6142a6ed79e
CodeWezus <tseguin@lixar.com>
2022-01-03 19:34:13 -0500

    Added nested routing for the Shop and Collection pages. Memoized selectCollection and collectionUrlParam

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,8 +1,28 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
+
+// An object used to match a string value (url parameters) to the correct integer id.
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
 
 const shopSelector = state => state.shop;
 
 export const selectCollections = createSelector(
     [shopSelector],
     shop => shop.collections
+);
+
+// Finds the collection.id which matches the url parameter of the collection id map. ie) shop/hats matches to hats: 1
+// By wrapping the function in a memoize we state that when this function receives collectionUrlParam we want to memoize the return of the function (a selector), 
+// if the function gets called with the same collectionUrlParam as last time, don't rerun the function but instead return the previous stored selector.
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    )
 );
